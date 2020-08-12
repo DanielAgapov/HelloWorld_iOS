97d65571112918641017a908f35f4e582be69aab
DanielAgapov <danielagapov1@gmail.com>
2020-08-12 04:57:47 -0400

    adding everything from first part of tutorial

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Daniel Agapov on 2020-08-11.
+//  Copyright © 2020 Daggerpov Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
